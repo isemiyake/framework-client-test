fb6407163f27dccd40de36ae956665942011121f
isemiyake <t.semih.pro@gmail.com>
2025-12-07 17:56:52 +0100

    Creation du store Products

---
+++ EAFC-MAGASIN/src/stores/products.js
@@ -0,0 +1,16 @@
+// src/stores/products.js
+import { defineStore } from 'pinia'
+import DB from '../services/DB.js' 
+
+export const useProductStore = defineStore('products', {
+  state: () => ({
+    products: []
+  }),
+
+  actions: {
+    async loadProducts() {
+      // On remplit le stock
+      this.products = await DB.findAll()
+    }
+  }
+})
